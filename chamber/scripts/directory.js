bbd808eafaef254270994dc540abba1e3b2024ea
YeiriMtz <yeirimartinez80@gmail.com>
2025-07-20 22:36:06 -0600

    jkjn

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -15,3 +15,14 @@ window.addEventListener("resize", () => {
     navLinks.classList.remove("show");
   }
 });
+
+// Highlight current page link
+const currentPage = location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(link => {
+  const linkPage = link.getAttribute("href");
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+}); 
